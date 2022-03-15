3e958ef9fc66e23856ce98a58e5f63c9bf34b31b
mktcode <kontakt@markus-kottlaender.de>
2022-03-15 04:05:59 +0100

    updated dist

--- dist/components.es.js
+++ dist/components.es.js
@@ -1,14 +1,12 @@
-import { openBlock, createElementBlock } from "vue";
-var _export_sfc = (sfc, props) => {
-  const target = sfc.__vccOpts || sfc;
-  for (const [key, val] of props) {
-    target[key] = val;
+import { defineComponent, openBlock, createElementBlock, toDisplayString } from "vue";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  props: {
+    title: { default: "A proposal" }
+  },
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("h1", null, toDisplayString(__props.title), 1);
+    };
   }
-  return target;
-};
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return openBlock(), createElementBlock("h1", null, "Proposal");
-}
-var Proposal = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
-export { Proposal };
+});
+export { _sfc_main as Proposal };
